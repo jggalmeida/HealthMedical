4f130af715a6496055cb5ec6d58c204aaa98a12d
jggalmeida <j.g.almeida@hotmail.com>
2020-11-19 00:57:40 -0300

    Arquivos de rotas, server e app criados

---
+++ Backend/src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+
+const routes = new Router();
+
+routes.get ('/', (req, res) => {
+    return res.json({ message: 'Hello World' });
+})
+
+export default routes;
