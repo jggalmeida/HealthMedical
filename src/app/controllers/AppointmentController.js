f3043147ce37db7c501328cbfaf9b689e77da31f
jggalmeida <j.g.almeida@hotmail.com>
2020-11-22 14:41:22 -0300

    Validação Agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,7 @@
-import Appointment from '../models/Appointment';
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
+import Appointment from '../models/Appointment';
 
 class AppointmentController{
     async store(req, res) {
@@ -24,10 +25,30 @@ class AppointmentController{
             return res.status(401).json({ error: 'Só é permitido criar agendamentos com médicos' });
         }
 
+        //Check for past dates
+        const hourStart = startOfHour(parseISO(date));
+
+        if(isBefore(hourStart, new Date())) {
+            return res.status(400).json({ error: 'Past dates are not permitted' });
+        }
+
+        //check date availability
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                medico_id,
+                canceled_at: null,
+                date: hourStart,
+            },
+        });
+
+        if(checkAvailability) {
+            return res.status(400).json({ error: 'Appointment date is not available' });
+        }
+
         const appointment = await Appointment.create({
             user_id: req.userId,
             medico_id,
-            date,
+            date: hourStart,
         });
 
         return res.json(appointment);
