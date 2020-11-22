d39f6cd75877cb678a35fb83719b9a90ff3f013d
jggalmeida <j.g.almeida@hotmail.com>
2020-11-22 01:40:44 -0300

    Middleware de Autenticação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,6 +17,12 @@ class UserController {
             user_type,
         });
     }
+
+    async update(req, res) {
+        console.log(req.userId);
+
+        return res.json({ ok: true });
+    }
 }
 
 export default new UserController();
